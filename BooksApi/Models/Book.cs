4c02ced05bbefd21d66630e9bcbf521f7f3b6518
Fadil.s <mohamed.fadil@ducenit.com>
2022-11-08 12:26:53 +0530

    API COMMIT

---
+++ BooksApi/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BooksApi.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string name { get; set; }
+
+        public string username { get; set; }
+
+        public string city { get; set; }
+
+        public string zip { get; set; }
+    }
+}
